3d893d7282b95c7aa1ddc8c1f8ddd2af17e31b2d
Sandra Tang <tangsandra9@gmail.com>
2025-10-16 09:46:16 -0400

    Changes to TOC

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -1,26 +1,30 @@
 "use client";
+import { usePathname } from "next/navigation";
+import Nav from "react-bootstrap/Nav";
 
-import { Nav } from "react-bootstrap";
 export default function TOC() {
- return (
-   <Nav variant="pills">
-     <Nav.Item>
-       <Nav.Link href="/Labs">Home</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="/Labs/Lab1">Lab 1</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="/Labs/Lab2" active>Lab 2</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="/Labs/Lab3">Lab 3</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-          <Nav.Link href="/Account/Signin">Kambaz</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="https://github.com/sandratangg">My GitHub</Nav.Link>
-     </Nav.Item>
-   </Nav>
-);}
+  const pathname = usePathname();
+
+  return (
+    <Nav variant="pills">
+      <Nav.Item>
+        <Nav.Link href="/Labs" active={pathname === "/Labs"}>Home</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/Labs/Lab1" active={pathname === "/Labs/Lab1"}>Lab 1</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/Labs/Lab2" active={pathname === "/Labs/Lab2"}>Lab 2</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/Labs/Lab3" active={pathname === "/Labs/Lab3"}>Lab 3</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/Account/Signin">Kambaz</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="https://github.com/sandratangg">My GitHub</Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
+}
